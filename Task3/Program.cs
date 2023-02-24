f19c0e7191994d0f6a5b7bf19503b44a58ba2721
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-02-24 17:48:27 +0300

    Task3: task is complete

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,3 +4,36 @@
 
 // 6, 1, 33 -> [6, 1, 33]
 
+int InputInt32(string msg)
+{
+    System.Console.WriteLine($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+
+}  
+
+int[] FillArray(int size, int MinElementLimit, int MaxElementLimit)
+{
+    int[] FilledArray = new int[size];
+    for (int i = 0; i < FilledArray.Length; i++)
+    {
+        FilledArray[i] = new Random().Next(MinElementLimit, MaxElementLimit);
+    }
+    return FilledArray;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Threading.Thread.Sleep(100);
+        System.Console.Write($" {array[i]} ");
+    }
+    System.Console.Write("]");
+}
+
+int size = InputInt32("Enter size of desired array");
+int MinLimit = InputInt32("Enter minimal element");
+int MaxLimit = InputInt32("Enter maximum element");
+int[] GeneratedArray = FillArray(size, MinLimit, MaxLimit);
+PrintArray(GeneratedArray);
